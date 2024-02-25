24711047d7debf2f160c5b1b639027497aaa2838
javiergenepaul <javiergenepaul@gmail.com>
2024-02-26 02:28:47 +0800

    improve minor details in header section

--- src/screens/hero/sections/header-section.tsx
+++ src/screens/hero/sections/header-section.tsx
@@ -2,6 +2,7 @@ import { BounceText, NavLinks, SocialButton, ThemeSwitch } from "@/components";
 import {
   NAV_LINKS,
   NavLinkInterface,
+  PATH,
   SOCIAL_MEDIA_LINKS,
   SocialMediaLinksInterface,
 } from "@/config";
@@ -9,15 +10,18 @@ import { translate } from "@/i18n";
 import { useThemeStore } from "@/stores";
 import React from "react";
 import { AboutMeAvatar, SubTitleAnimation } from "../components";
+import { Link } from "react-router-dom";
 
 export const HeaderSection = () => {
   const { getTheme, toggleTheme } = useThemeStore();
   return (
     <>
       <section>
-        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-          <BounceText text={translate("header.name")} />
-        </h1>
+        <Link to={PATH.ABOUT.path}>
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+            <BounceText text={translate("header.name")} />
+          </h1>
+        </Link>
         <SubTitleAnimation />
         <p className="mt-4 leading-normal">{translate("header.description")}</p>
         <nav
