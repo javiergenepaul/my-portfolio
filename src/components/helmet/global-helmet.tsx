fc599fb2371948ea69377e76c0f7ecab21317786
javiergenepaul <javiergenepaul@gmail.com>
2024-02-26 01:28:50 +0800

    add a common for setting page title & add routes for other pages

--- src/components/helmet/global-helmet.tsx
+++ src/components/helmet/global-helmet.tsx
@@ -1,9 +1,9 @@
-import { APP_NAME } from "@/config";
+import { translate } from "@/i18n";
 import { usePageTitleStore } from "@/stores";
 import { Helmet as ReactHelmet } from "react-helmet";
 
 export const GlobalHelmet = () => {
   const { title } = usePageTitleStore();
 
-  return <ReactHelmet title={`${APP_NAME} - ${title}`} />;
+  return <ReactHelmet title={translate("appName", { title: title })} />;
 };
--- src/config/properties.ts
+++ src/config/properties.ts
@@ -1,3 +1,2 @@
-export const APP_NAME: string = "Javs Portfolio";
 export const DEV_MODE: "development" | "production" = import.meta.env
   .VITE_DEV_MODE;
