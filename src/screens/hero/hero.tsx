4ec4fec2c67cc8c7d481546aeb7e55ad9de754f2
javiergenepaul <javiergenepaul@gmail.com>
2024-02-26 00:38:49 +0800

    on scroll event feature for nav-links

--- src/screens/hero/hero.tsx
+++ src/screens/hero/hero.tsx
@@ -1,4 +1,4 @@
-import { usePageTitleStore } from "@/stores";
+import { SelectedNavLink, useNavLinkStore, usePageTitleStore } from "@/stores";
 import { useEffect } from "react";
 import {
   ContactSection,
@@ -10,17 +10,63 @@ import {
 
 export const Hero = () => {
   const { setTitle } = usePageTitleStore();
+  const { setSelectedNav, setOnScrollNav } = useNavLinkStore();
+
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
 
   useEffect(() => {
     setTitle("Home Page");
+    setSelectedNav("services");
   }, []);
+
+  const isSectionPartiallyInView = (data: {
+    sectionTop: number;
+    sectionBottom: number;
+  }): boolean => {
+    const { sectionTop, sectionBottom } = data;
+    const sectionHeight = sectionBottom - sectionTop;
+    const margin = sectionHeight * 0.2;
+
+    const inside: boolean =
+      sectionTop + margin < scrollY && sectionBottom + margin > scrollY;
+
+    return inside;
+  };
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      const sections = document.querySelectorAll<HTMLElement>(".section");
+      sections.forEach((section: HTMLElement) => {
+        if (isInViewport(section)) {
+          setOnScrollNav(section.id as SelectedNavLink);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isInViewport = (element: HTMLElement): boolean => {
+    const rect = element.getBoundingClientRect();
+
+    return isSectionPartiallyInView({
+      sectionTop: rect.top,
+      sectionBottom: rect.bottom,
+    });
+  };
 
   return (
     <div className="lg:flex lg:justify-between lg:gap-4">
       <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-2/5 lg:flex-col lg:justify-between lg:py-24">
         <HeaderSection />
       </header>
-      <main className="lg:w-3/5 lg:min-h-screen flex flex-col">
+      <main className="flex flex-col lg:w-3/5 lg:min-h-screen snap-y">
         <ServiceSection />
         <ProjectSection />
         <ContactSection />
--- src/screens/hero/sections/service-section.tsx
+++ src/screens/hero/sections/service-section.tsx
@@ -3,7 +3,7 @@ import { translate } from "@/i18n";
 export const ServiceSection = () => {
   return (
     <section
-      className="h-screen bg-blue-800"
+      className="h-screen bg-blue-800 section snap-start"
       id="services"
       aria-label={translate("header.ariaLabel.serviceSection")}
     >
