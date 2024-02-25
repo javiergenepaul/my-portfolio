f4487038069f89ba6ebb2a3f9ccfa9aaee571f63
javiergenepaul <javiergenepaul@gmail.com>
2024-02-26 00:38:49 +0800

    on scroll event feature for nav-links

--- src/stores/nav-link-store.tsx
+++ src/stores/nav-link-store.tsx
@@ -4,13 +4,20 @@ type SelectedNavLink = "services" | "projects" | "contacts";
 interface NavLinkStoreInterface {
   selectedNav: SelectedNavLink;
   setSelectedNav: (selectedNav: SelectedNavLink) => void;
+  setOnScrollNav: (selectedNav: SelectedNavLink) => void;
 }
 
-const useNavLinkStore = create<NavLinkStoreInterface>((set) => {
+const useNavLinkStore = create<NavLinkStoreInterface>((set, get) => {
   return {
     selectedNav: "services",
     setSelectedNav: (value: SelectedNavLink) =>
       set(() => ({ selectedNav: value })),
+    setOnScrollNav: (value: SelectedNavLink) => {
+      if (get().selectedNav === value) {
+        return;
+      }
+      set(() => ({ selectedNav: value }));
+    },
   };
 });
 
