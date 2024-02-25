a8ab0006935d7671d2629b318490554be50a8140
javiergenepaul <javiergenepaul@gmail.com>
2024-02-25 23:21:56 +0800

    installation & setup for tailwind merge and solve the navlink proble

--- src/components/ui/nav-links.tsx
+++ src/components/ui/nav-links.tsx
@@ -1,5 +1,6 @@
 import { NavLinkInterface } from "@/config";
 import { useNavLinkStore } from "@/stores";
+import { twMerge } from "tailwind-merge";
 
 export const NavLinks = (props: NavLinkInterface) => {
   const { key, name, path, selectedId } = props;
@@ -9,7 +10,7 @@ export const NavLinks = (props: NavLinkInterface) => {
   const selectedClass = (): { lineClass: string; nameClass: string } => {
     return {
       lineClass: isActive ? "w-16 bg-white" : "bg-slate-400",
-      nameClass: isActive ? "text-slate-200 text-primary" : "text-slate-400",
+      nameClass: isActive ? "text-slate-200 text-white" : "text-slate-400",
     };
   };
 
@@ -24,14 +25,16 @@ export const NavLinks = (props: NavLinkInterface) => {
         href={path}
       >
         <span
-          className={`nav-indicator mr-4 h-px w-8 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none ${
+          className={twMerge(
+            "nav-indicator mr-4 h-px w-8 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none",
             selectedClass().lineClass
-          }`}
+          )}
         />
         <span
-          className={`nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200 ${
+          className={twMerge(
+            "nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200",
             selectedClass().nameClass
-          }`}
+          )}
         >
           {name}
         </span>
