632d6d85328b9438f81f56867ce2d73ddac8f003
javiergenepaul <javiergenepaul@gmail.com>
2024-02-28 01:02:38 +0800

    add popover in social media icons

--- src/components/ui/social-button.tsx
+++ src/components/ui/social-button.tsx
@@ -1,17 +1,23 @@
 import { SocialMediaLinksInterface } from "@/config";
 import { Icon } from "@iconify/react";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from ".";
 
 export const SocialButton = (props: SocialMediaLinksInterface) => {
   const { icon, title, url } = props;
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noreferrer"
-      aria-label={title}
-      className="flex flex-row items-center gap-2 transition duration-150"
-    >
-      <Icon icon={icon as any} width="32" height="32" />
-    </a>
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <a
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          aria-label={title}
+          className="flex flex-row items-center gap-2 transition duration-150"
+        >
+          <Icon icon={icon as any} width="32" height="32" />
+        </a>
+      </HoverCardTrigger>
+      <HoverCardContent side="top" className="flex justify-center px-8 py-2.5 w-fit">{title}</HoverCardContent>
+    </HoverCard>
   );
 };
