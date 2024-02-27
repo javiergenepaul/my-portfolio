22cb4d158e0f0439e275ae201452e20ec34add64
javiergenepaul <javiergenepaul@gmail.com>
2024-02-28 01:21:52 +0800

    add tooltip for social icons and about me icon

--- src/screens/hero/components/about-me-avatar.tsx
+++ src/screens/hero/components/about-me-avatar.tsx
@@ -1,35 +1,36 @@
 import {
   Avatar,
-  AvatarFallback,
+  Tooltip,
   AvatarImage,
-  HoverCardContent,
+  TooltipTrigger,
+  AvatarFallback,
+  TooltipContent,
+  TooltipProvider,
 } from "@/components";
 import { PATH } from "@/config";
 import { Link } from "react-router-dom";
 import ProfileAvater from "../../../assets/avatar-profile.jpg";
 import { translate } from "@/i18n";
-import { HoverCard, HoverCardTrigger } from "@radix-ui/react-hover-card";
 
 export const AboutMeAvatar = () => {
   return (
-    <HoverCard>
-      <HoverCardTrigger>
-        <Link
-          to={PATH.ABOUT.path}
-          className="flex items-center justify-center gap-2 cursor-pointer"
-        >
-          <Avatar>
-            <AvatarImage src={ProfileAvater} />
-            <AvatarFallback>{translate("header.shortName")}</AvatarFallback>
-          </Avatar>
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent
-        side="top"
-        className="flex justify-center w-fit py-2.5 px-8"
-      >
-        {translate("header.aboutMe")}
-      </HoverCardContent>
-    </HoverCard>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Link
+            to={PATH.ABOUT.path}
+            className="flex items-center justify-center gap-2 cursor-pointer"
+          >
+            <Avatar>
+              <AvatarImage src={ProfileAvater} />
+              <AvatarFallback>{translate("header.shortName")}</AvatarFallback>
+            </Avatar>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <span>{translate("header.aboutMe")}</span>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
