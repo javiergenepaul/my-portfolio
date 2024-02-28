a0b77ed862b4800692beeb5a393cd7d418b3ea45
javiergenepaul <javiergenepaul@gmail.com>
2024-02-28 18:33:41 +0800

    applying best practices in hero components folder

---
+++ src/screens/hero/components/component-props.ts
@@ -0,0 +1,25 @@
+import { ServiceOfferInterface, TechStackInterface } from "@/config";
+import { SelectedNavLink } from "@/stores";
+
+export interface ServiceCardInterface extends ServiceOfferInterface {}
+
+export interface HighlightTextInterface {
+  url: string;
+  text: string;
+}
+
+export interface NavLinkInterface {
+  key: SelectedNavLink;
+  name: string;
+  path: string;
+  selectedId: SelectedNavLink;
+}
+
+export interface SocialMediaLinksInterface {
+  key: string;
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface StackDetailsProps extends TechStackInterface {}
--- src/screens/hero/components/service-card.tsx
+++ src/screens/hero/components/service-card.tsx
@@ -5,21 +5,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components";
-import { ServiceOfferInterface, TechStackInterface } from "@/config";
-// import { useState } from "react";
-// import { motion } from "framer-motion";
-// import { translate } from "@/i18n";
+import { TechStackInterface } from "@/config";
 import { StackContent } from "./stack-content";
-
-interface ServiceCardInterface extends ServiceOfferInterface {}
+import { ServiceCardInterface } from "./component-props";
 
 export const ServiceCard = (props: ServiceCardInterface) => {
   const { title, description, stack } = props;
-  // const [showStack, setShowStack] = useState<boolean>(false);
-
-  // const toggleVisibility = () => {
-  //   setShowStack(!showStack);
-  // };
 
   return (
     <Card className="border-transparent select-none bg-white/[5%] backdrop-blur-sm drop-shadow-lg">
@@ -32,35 +23,6 @@ export const ServiceCard = (props: ServiceCardInterface) => {
           return <StackContent {...stack} />;
         })}
       </CardFooter>
-      {/* {stack && stack?.length > 10 ? (
-        <CardFooter className="flex flex-wrap gap-2">
-          <motion.div
-            className={"flex flex-wrap gap-x-1.5 gap-y-0.5 items-center"}
-            initial={{ opacity: 1 }}
-            style={{ display: !showStack ? "" : "flex" }}
-            animate={{
-              opacity: showStack ? 0 : 1,
-              ...(!showStack ? {} : { transitionEnd: { display: "none" } }),
-            }}
-            transition={{ duration: 0.2 }}
-          >
-            {stack?.map((stack: TechStackInterface) => {
-              return <StackContent {...stack} />;
-            })}
-          </motion.div>
-          <Button className="p-0" variant="link" onClick={toggleVisibility}>
-            <small>
-              {showStack ? translate("more.show") : translate("more.hide")}
-            </small>
-          </Button>
-        </CardFooter>
-      ) : (
-        <CardFooter className="flex flex-wrap gap-2">
-          {stack?.map((stack: TechStackInterface) => {
-            return <StackContent {...stack} />;
-          })}
-        </CardFooter>
-      )} */}
     </Card>
   );
 };
