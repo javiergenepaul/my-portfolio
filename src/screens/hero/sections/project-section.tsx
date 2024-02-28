56d45563cdcbde2985a8acbbc495b0366341e40d
javiergenepaul <javiergenepaul@gmail.com>
2024-02-29 03:04:40 +0800

    update project section

--- src/screens/hero/sections/project-section.tsx
+++ src/screens/hero/sections/project-section.tsx
@@ -2,7 +2,7 @@ export const ProjectSection = () => {
   return (
     <section
       id="projects"
-      className="h-screen bg-green-400 section snap-start"
+      className="h-screen py-16 bg-green-400 section snap-start lg:py-24"
       aria-label="Projects"
     >
       <span className="text-white">Project Section</span>
