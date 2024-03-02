0ec81798a5ce73077b9585edb5f2a856592f9cf1
javiergenepaul <javiergenepaul@gmail.com>
2024-03-03 04:24:48 +0800

    add header to all pages

--- src/components/ui/page-title.tsx
+++ src/components/ui/page-title.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Separator } from ".";
+import { translate } from "@/i18n";
 
 interface PageTitleInterface {
   pageName: string;
@@ -15,12 +16,12 @@ export const PageTitle = (props: PageTitleInterface) => {
       <div className="space-y-0.5">
         <Link
           to={path}
-          className="inline-flex items-center mb-2 font-semibold leading-tight text-teal-300 cursor-pointer group"
+          className="inline-flex items-center mb-2 font-semibold leading-tight cursor-pointer text-primary group"
         >
           {
             <ArrowLeft className="w-4 h-4 mr-1 transition-transform group-hover:-translate-x-2" />
           }
-          Paul Javier
+          {translate("header.nickName")}
         </Link>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
           {pageName}
