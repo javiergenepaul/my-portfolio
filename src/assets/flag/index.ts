ea9f54aa423535db2c02e3820acc29b916ce2d3c
javiergenepaul <javiergenepaul@gmail.com>
2024-03-02 23:29:48 +0800

    add flag image for the flag option in sidebar

---
+++ src/assets/flag/index.ts
@@ -0,0 +1,5 @@
+import USFlag from "./us.svg";
+import PHFlag from "./ph.svg";
+import JPFlag from "./jp.svg";
+
+export { USFlag, PHFlag, JPFlag };
--- src/components/sidebar/component/sidebar-overlay.tsx
+++ src/components/sidebar/component/sidebar-overlay.tsx
@@ -13,7 +13,7 @@ export const SidebarOverlay = () => {
     <Drawer.Overlay
       {...handlers}
       onClick={() => setIsOpen(false)}
-      className="fixed inset-0 bg-black/10 dark:bg-white/10 backdrop-blur-sm z-[60]"
+      className="fixed inset-0 bg-black/60 dark:bg-white/10 dark:backdrop-blur-[2px] backdrop-blur-0 z-[60]"
     />
   );
 };
