0be57a9b49b4cbe0af69222e360e5f374ac83364
javiergenepaul <javiergenepaul@gmail.com>
2024-03-03 04:24:48 +0800

    add header to all pages

--- src/screens/project/project.tsx
+++ src/screens/project/project.tsx
@@ -1,9 +1,18 @@
-import { FadeAnimation } from "@/components";
+import { FadeAnimation, PageTitle } from "@/components";
+import { PATH } from "@/config";
+import { translate } from "@/i18n";
 
 export const Project = () => {
   return (
     <FadeAnimation>
-      <div>Projects</div>
+      <div className="py-10 pt-0 pb-16 space-y-6 lg:pt-10 ">
+        <PageTitle
+          pageName={translate("projects.title")}
+          path={PATH.HOME.path}
+          description={translate("projects.description")}
+        />
+        <h1>{translate("availableSoon")}</h1>
+      </div>
     </FadeAnimation>
   );
 };
