05584f8e664f48a7dea904e7af470e70984f00e7
javiergenepaul <javiergenepaul@gmail.com>
2024-03-03 22:46:47 +0800

    resolve error on deployig

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,6 @@ import AvatarProfile from "./avatar-profile.jpg";
 import ResumeDark from "./resume_dark.pdf";
 import ResumeLight from "./resume_light.pdf";
 import ContactLogo from "./contact-logo.svg";
-import MetaBG from "./meta-bg.png";
 
 export * from "./flag";
-export { AvatarProfile, ResumeLight, ResumeDark, ContactLogo, MetaBG };
+export { AvatarProfile, ResumeLight, ResumeDark, ContactLogo };
--- src/components/helmet/global-helmet.tsx
+++ src/components/helmet/global-helmet.tsx
@@ -1,5 +1,3 @@
-import { MetaBG } from "@/assets";
-import { PATH } from "@/config";
 import { translate } from "@/i18n";
 import { usePageTitleStore } from "@/stores";
 import { Helmet as ReactHelmet } from "react-helmet";
@@ -7,12 +5,5 @@ import { Helmet as ReactHelmet } from "react-helmet";
 export const GlobalHelmet = () => {
   const { title } = usePageTitleStore();
 
-  return (
-    <ReactHelmet title={translate("appName", { title: title })}>
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={"test description"} />
-      <meta property="og:image" content={MetaBG} />
-      <meta property="og:url" content={PATH.HOME.path} />
-    </ReactHelmet>
-  );
+  return <ReactHelmet title={translate("appName", { title: title })} />;
 };
