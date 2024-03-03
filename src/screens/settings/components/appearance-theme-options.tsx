21b7e09bb3d235f1f5673b72bb74c30e9512c102
javiergenepaul <javiergenepaul@gmail.com>
2024-03-03 21:14:35 +0800

    remove commented code

--- src/screens/settings/components/appearance-theme-options.tsx
+++ src/screens/settings/components/appearance-theme-options.tsx
@@ -82,29 +82,6 @@ export const ApperanceThemeOption = (props: ApperanceThemeOptionInterface) => {
         className="[&:has([data-state=checked])>div]:border-primary"
       >
         {RenderSelector()}
-        {/* <ApperanceThemeSelector
-          theme={theme}
-          borderColor={borderColor}
-          popoverColor={popoverColor}
-          hoverColor={hoverColor}
-          textColor={textColor}
-        /> */}
-        {/* <div className="items-center p-1 border-2 rounded-md border-muted bg-popover hover:bg-accent hover:text-accent-foreground">
-          <div className="p-2 space-y-2 rounded-sm bg-slate-950">
-            <div className="p-2 space-y-2 rounded-md shadow-sm bg-slate-800">
-              <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-            </div>
-            <div className="flex items-center p-2 space-x-2 rounded-md shadow-sm bg-slate-800">
-              <div className="w-4 h-4 rounded-full bg-slate-400" />
-              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-            </div>
-            <div className="flex items-center p-2 space-x-2 rounded-md shadow-sm bg-slate-800">
-              <div className="w-4 h-4 rounded-full bg-slate-400" />
-              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-            </div>
-          </div>
-        </div> */}
         <span className="block w-full p-2 font-normal text-center">{name}</span>
       </Label>
     </div>
