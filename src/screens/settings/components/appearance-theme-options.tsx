218bdf15c7c7071eb68249b2083b0367fc39425a
javiergenepaul <javiergenepaul@gmail.com>
2024-03-05 10:19:52 +0800

    change content in emerald

--- src/screens/settings/components/appearance-theme-options.tsx
+++ src/screens/settings/components/appearance-theme-options.tsx
@@ -19,7 +19,7 @@ export const ApperanceThemeOption = (props: ApperanceThemeOptionInterface) => {
       />
       <Label
         htmlFor={value}
-        className="flex gap-4 flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+        className="flex gap-4 flex-col items-center text-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
       >
         {icon}
         {name}
