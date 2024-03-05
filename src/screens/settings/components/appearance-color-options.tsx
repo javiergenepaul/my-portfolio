8cc72994398d9bd5258b11935eb4e2e5477255ed
javiergenepaul <javiergenepaul@gmail.com>
2024-03-05 08:39:22 +0800

    update the design on color palette options

--- src/screens/settings/components/appearance-color-options.tsx
+++ src/screens/settings/components/appearance-color-options.tsx
@@ -1,6 +1,5 @@
 import { Label, RadioGroupItem } from "@/components";
 import { Color } from "@/stores";
-import { twMerge } from "tailwind-merge";
 
 export interface AppearanceColorOptionsInterface {
   value: Color;
@@ -14,7 +13,6 @@ export const AppearanceColorOptions = (
   const { name, value } = props;
   return (
     <div>
-      {/* TODO:: CHANGE DESIGN HERE */}
       <RadioGroupItem
         value={value as string}
         id={value}
@@ -22,9 +20,9 @@ export const AppearanceColorOptions = (
       />
       <Label
         htmlFor={value}
-        className="flex gap-4 flex-col items-center justify-between text-center  rounded-full border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+        className="flex gap-4 flex-col items-center justify-between text-center rounded-full border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
       >
-        <span className={twMerge("", `text-red`)}>{name}</span>
+        <span className={`text-${value}`}>{name}</span>
       </Label>
     </div>
   );
