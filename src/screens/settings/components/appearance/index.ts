eb4c9fae2ea93e867d28b9eaf8fb621a2f8448eb
javiergenepaul <javiergenepaul@gmail.com>
2024-03-10 12:49:47 +0800

    refactor settings component directory

---
+++ src/screens/settings/components/appearance/index.ts
@@ -0,0 +1,4 @@
+export * from "./appearance-color-options";
+export * from "./appearance-theme-options-skeleton";
+export * from "./appearance-theme-options";
+export * from "./apperance-font-options";
