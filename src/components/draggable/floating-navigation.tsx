35c4905813483289cb45ecfe35878492d4a9dca8
javiergenepaul <javiergenepaul@gmail.com>
2024-03-10 13:44:02 +0800

    refactor files for floating settings

--- src/components/draggable/floating-navigation.tsx
+++ src/components/draggable/floating-navigation.tsx
@@ -1,83 +1,44 @@
 import { useState } from "react";
 import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { twMerge } from "tailwind-merge";
-
-interface Distances {
-  [key: string]: number;
-  top: number;
-  left: number;
-  bottom: number;
-  right: number;
-}
+import { Label, Switch } from "..";
 
 export const FloatingNavigation = () => {
   const [onDrag, setOnDrag] = useState<boolean>(false);
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  });
 
   const onStartHandler = () => {
     setOnDrag(true);
   };
 
-  const onDragHandler = (_e: DraggableEvent, _data: DraggableData) => {
-    // console.log("Dragging", data.x, data.y);
-  };
+  const onDragHandler = (_e: DraggableEvent, _data: DraggableData) => {};
 
-  const onStopHandler = (_e: DraggableEvent, data: DraggableData) => {
-    const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
-    const elementWidth = data.node.clientWidth;
-    const elementHeight = data.node.clientHeight;
-
-    // Define distances with an index signature
-    const distances: Distances = {
-      top: data.y,
-      left: data.x,
-      bottom: viewportHeight - (data.y + elementHeight),
-      right: viewportWidth - (data.x + elementWidth),
-    };
-    // Determine the nearest edge
-    const nearestEdge = Object.keys(distances).reduce((a, b) =>
-      distances[a] < distances[b] ? a : b
-    );
-
-    // Calculate new position based on nearest edge
-    let newPosition = { x: data.x, y: data.y };
-    switch (nearestEdge) {
-      case "top":
-        newPosition.y = 0;
-        break;
-      case "left":
-        newPosition.x = 0;
-        break;
-      case "bottom":
-        newPosition.y = viewportHeight - elementHeight;
-        break;
-      case "right":
-        newPosition.x = viewportWidth - elementWidth;
-        break;
-    }
-    setPosition(newPosition);
+  const onStopHandler = (_e: DraggableEvent, _data: DraggableData) => {
+    setOnDrag(false);
   };
 
   return (
-    <Draggable
-      position={position}
-      bounds="parent"
-      onStart={onStartHandler}
-      onDrag={onDragHandler}
-      onStop={onStopHandler}
-    >
-      <div
-        className={twMerge(
-          "bg-white z-[999999999999] p-4 rounded-lg shadow-lg cursor-grab pointer-events-auto",
-          onDrag ? "cursor-grabbing" : ""
-        )}
+    <div className="fixed inset-0 z-50 flex justify-center items-center pointer-events-none">
+      <Draggable
+        bounds="parent"
+        onStart={onStartHandler}
+        onDrag={onDragHandler}
+        onStop={onStopHandler}
       >
-        <h1 className="text-3xl text-black capitalize">test drag me</h1>\
-      </div>
-    </Draggable>
+        <div
+          className={twMerge(
+            "bg-popover z-[999999999999] rounded-lg shadow-lg cursor-grab pointer-events-auto",
+            onDrag ? "cursor-grabbing" : ""
+          )}
+        >
+          <div className="p-4 flex flex-col gap-2">
+            <div className="Setting">Settings</div>
+            <div className="flex items-center gap-2">
+              <Switch id="test" />
+              <Label htmlFor="test">Show Background Only</Label>
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    </div>
   );
 };
