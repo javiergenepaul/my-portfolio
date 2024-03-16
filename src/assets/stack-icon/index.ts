af11fc7ce1371ad11326424fb4cc77b7c5228d3b
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 10:24:23 +0800

    add icon for nginx stack ang git tech

--- src/assets/stack-icon/index.ts
+++ src/assets/stack-icon/index.ts
@@ -1,9 +1,11 @@
 import ApiStackIcon from "./api-stack-icon.svg";
 import AzureStackIcon from "./azure-stack-icon.svg";
 import FirebaseStackIcon from "./firebase-stack-icon.svg";
+import GitStackIcon from "./git-stack-icon.svg";
 import JavaStackIcon from "./java-stack-icon.svg";
 import LinuxStackIcon from "./linux-stack-icon.svg";
 import MySQLStackIcon from "./mysql-stack-icon.svg";
+import NginxStackIcon from "./nginx-stack-icon.svg";
 import PostmanStackIcon from "./postman-stack-icon.svg";
 import ReactStackIcon from "./react-stack-icon.svg";
 import SpringBootStackIcon from "./spring-boot-stack-icon.svg";
@@ -14,9 +16,11 @@ export {
   ApiStackIcon,
   AzureStackIcon,
   FirebaseStackIcon,
+  GitStackIcon,
   JavaStackIcon,
   LinuxStackIcon,
   MySQLStackIcon,
+  NginxStackIcon,
   PostmanStackIcon,
   ReactStackIcon,
   SpringBootStackIcon,
