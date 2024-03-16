ee06df1c5c4e46183e1d2e457ec5fcce102eb88d
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 11:28:38 +0800

    add vite stack iconf or hover card badge

--- src/assets/stack-icon/index.ts
+++ src/assets/stack-icon/index.ts
@@ -4,16 +4,19 @@ import CentosStackIcon from "./centos-stack-icon.svg";
 import FirebaseStackIcon from "./firebase-stack-icon.svg";
 import GitStackIcon from "./git-stack-icon.svg";
 import JavaStackIcon from "./java-stack-icon.svg";
+import LaravelStackIcon from "./laravel-stack-icon.svg";
 import LinuxStackIcon from "./linux-stack-icon.svg";
 import MVCStackIcon from "./mvc-stack-icon.svg";
 import MySQLStackIcon from "./mysql-stack-icon.svg";
 import NetlifyStackIcon from "./netlify-stack-icon.svg";
 import NginxStackIcon from "./nginx-stack-icon.svg";
+import PHPStackIcon from "./php-stack-icon.svg";
 import PostmanStackIcon from "./postman-stack-icon.svg";
 import ReactStackIcon from "./react-stack-icon.svg";
 import SpringBootStackIcon from "./spring-boot-stack-icon.svg";
 import SpringSecurityStackIcon from "./spring-security-stack-icon.svg";
 import SpringJDBCStackIcon from "./spring-jdbc-stack-icon.svg";
+import ViteStackIcon from "./vite-stack-icon.svg";
 
 export {
   ApiStackIcon,
@@ -22,15 +25,17 @@ export {
   FirebaseStackIcon,
   GitStackIcon,
   JavaStackIcon,
+  LaravelStackIcon,
   LinuxStackIcon,
   MVCStackIcon,
   MySQLStackIcon,
   NetlifyStackIcon,
   NginxStackIcon,
+  PHPStackIcon,
   PostmanStackIcon,
   ReactStackIcon,
   SpringBootStackIcon,
   SpringSecurityStackIcon,
   SpringJDBCStackIcon,
+  ViteStackIcon,
 };
-``;
