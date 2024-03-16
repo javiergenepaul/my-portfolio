e8103120bc0ae7df8bf1399c808c9e9ed6f02070
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 20:37:44 +0800

    add floating nav bar in skills page

--- src/screens/skills/skills.tsx
+++ src/screens/skills/skills.tsx
@@ -1,10 +1,11 @@
-import { FadeAnimation, PageTitle } from "@/components";
+import { FadeAnimation, FloatingNavbar, PageTitle } from "@/components";
 import { PATH } from "@/config";
 import { translate } from "@/i18n";
 
 export const Skills = () => {
   return (
     <FadeAnimation>
+      <FloatingNavbar />
       <div className="py-10 pt-0 pb-16 space-y-6 lg:pt-10 ">
         <PageTitle
           pageName={translate("skills.title")}
