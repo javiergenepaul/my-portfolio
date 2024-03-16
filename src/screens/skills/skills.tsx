4f11820054654f7093380964d61f6eabeaed2ebe
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 20:38:25 +0800

    add floating navbar in settings page

--- src/screens/skills/skills.tsx
+++ src/screens/skills/skills.tsx
@@ -5,7 +5,7 @@ import { translate } from "@/i18n";
 export const Skills = () => {
   return (
     <FadeAnimation>
-      <FloatingNavbar />
+      <FloatingNavbar />  
       <div className="py-10 pt-0 pb-16 space-y-6 lg:pt-10 ">
         <PageTitle
           pageName={translate("skills.title")}
