3f0308760564e9f03aad356d0745afbb68e47dc0
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 17:00:14 +0800

    add social icon in sidebar

--- src/components/sidebar/component/sidebar-content.tsx
+++ src/components/sidebar/component/sidebar-content.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { PATH, SideMenuInterface } from "@/config";
+import { PATH, SOCIAL_MEDIA_LINKS, SideMenuInterface } from "@/config";
 import { PersonIcon } from "@radix-ui/react-icons";
 import { HomeIcon, PieChart, Contact, Code, Settings } from "lucide-react";
 import { Drawer } from "vaul";
 import { translate } from "@/i18n";
 import { SidebarMenu } from "./sidebar-menu";
+import { SocialMediaLinksInterface } from "@/screens";
+import { SocialIcon } from "@/components";
 
 interface SidebarContentInterface {
   title: string;
@@ -50,16 +52,29 @@ export const SidebarContent = (props: SidebarContentInterface) => {
 
   return (
     <Drawer.Content className="bg-foreground flex flex-col rounded-t-[10px] h-full w-[325px] z-[70] mt-24 fixed bottom-0 right-0">
-      <div className="flex-1 h-full p-4 pb-16 bg-background">
-        <div className="h-full max-w-md mx-auto">
-          <Drawer.Title className="px-2 mb-4 font-medium">{title}</Drawer.Title>
-          <div className="flex flex-col justify-between h-full px-2 py-4">
-            <div className="">
-              {SIDE_MENU.map((menu: SideMenuInterface, index: React.Key) => (
-                <React.Fragment key={index}>
-                  <SidebarMenu {...menu} />
-                </React.Fragment>
-              ))}
+      <div className="flex-1 h-screen p-4 bg-background">
+        <div className="h-full max-w-md mx-auto flex flex-col">
+          <div className="flex flex-col gap-4">
+            <Drawer.Title className="px-2 font-medium">{title}</Drawer.Title>
+            <div className="flex flex-col justify-between px-2">
+              <div>
+                {SIDE_MENU.map((menu: SideMenuInterface, index: React.Key) => (
+                  <React.Fragment key={index}>
+                    <SidebarMenu {...menu} />
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-auto pt-auto px-2">
+            <div className="flex justify-end gap-4">
+              {SOCIAL_MEDIA_LINKS.map(
+                (socialMedia: SocialMediaLinksInterface, index: React.Key) => (
+                  <a className="flex gap-2" key={index} href={socialMedia.url}>
+                    <SocialIcon icon={socialMedia.icon} />
+                  </a>
+                )
+              )}
             </div>
           </div>
         </div>
