e67ba6a97c34d8e103861efb4340a464cc452550
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 18:28:42 +0800

    add method to calculate all project status

--- src/screens/about/sections/intro-section.tsx
+++ src/screens/about/sections/intro-section.tsx
@@ -1,13 +1,54 @@
 import { AboutMe, ResumeDark } from "@/assets";
 import { Button } from "@/components";
+import { ProjectInterface, ProjectStatus } from "@/config";
 import { SubTitleAnimation } from "@/screens";
+import { PROJECTS } from "@/screens/hero/sections";
 import { DownloadIcon } from "@radix-ui/react-icons";
+import moment from "moment";
+
+interface PersonalStatisticInterface {
+  count: number;
+  topTitle: string;
+  botTitle: string;
+}
 
 export const IntroSection = () => {
   const DownloadResumeHandler = () => {
     console.log("download resume here");
     window.open(ResumeDark, "_blank");
   };
+
+  /**
+   * Counts the number of completed projects.
+   * @returns {number} The count of completed projects.
+   */
+  const countProjectStatus = (status: ProjectStatus): number => {
+    let count: number = 0;
+    PROJECTS.forEach((project: ProjectInterface) => {
+      if (project.stack && project.status === status) {
+        count++;
+      }
+    });
+    return count;
+  };
+
+  const STATISTICS: PersonalStatisticInterface[] = [
+    {
+      count: countProjectStatus("ongoing"),
+      topTitle: "Ongoing",
+      botTitle: "Project",
+    },
+    {
+      count: countProjectStatus("completed"),
+      topTitle: "Project",
+      botTitle: "Completed",
+    },
+    {
+      count: moment().diff(moment("2018-08-01"), "years"),
+      topTitle: "Years",
+      botTitle: "Experience",
+    },
+  ];
 
   return (
     <div className="h-screen flex">
@@ -42,6 +83,45 @@ export const IntroSection = () => {
             <span>Download My Resume</span>
             <DownloadIcon />
           </Button>
+
+          <div className="grid grid-cols-3 mt-8">
+            {STATISTICS.map((item: PersonalStatisticInterface) => {
+              return (
+                <div className="flex flex-col">
+                  <h1 className="text-5xl font-semibold">{item.count}+</h1>
+                  <span className="text-sm text-muted-foreground">
+                    {item.topTitle}
+                    <br />
+                    {item.botTitle}
+                  </span>
+                </div>
+              );
+            })}
+            {/* <div className="flex flex-col">
+              <h1 className="text-5xl">10+</h1>
+              <span className="text-sm text-muted-foreground">
+                Project
+                <br />
+                Completed
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-5xl">5+</h1>
+              <span className="text-sm text-muted-foreground">
+                Years
+                <br />
+                Experience
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-5xl">2+</h1>
+              <span className="text-sm text-muted-foreground">
+                Awards
+                <br />
+                Achievement
+              </span>
+            </div> */}
+          </div>
         </div>
       </div>
     </div>
