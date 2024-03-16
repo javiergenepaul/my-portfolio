c768490101cd9f4f608eb307d429a7cd8cba6c77
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 18:05:25 +0800

    add image in about me section and download resume function

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,8 +2,9 @@ import AvatarProfile from "./avatar-profile.jpg";
 import ResumeDark from "./resume_dark.pdf";
 import ResumeLight from "./resume_light.pdf";
 import ContactLogo from "./contact-logo.svg";
+import AboutMe from "./about-me.png";
 
 export * from "./flag";
 export * from "./projects";
 export * from "./stack-icon";
-export { AvatarProfile, ResumeLight, ResumeDark, ContactLogo };
+export { AvatarProfile, ResumeLight, ResumeDark, ContactLogo, AboutMe };
