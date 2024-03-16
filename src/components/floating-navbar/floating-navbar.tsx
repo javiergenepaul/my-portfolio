1f0e4c4922c57938dc276a8757916cb3a2deac4c
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 20:41:26 +0800

    add pointer events in floating nav bar

--- src/components/floating-navbar/floating-navbar.tsx
+++ src/components/floating-navbar/floating-navbar.tsx
@@ -39,9 +39,9 @@ export const FloatingNavbar = () => {
   ];
 
   return (
-    <div className="fixed inset-0 h-full w-full flex items-center">
-      <div className="bg-popover py-4 px-2 rounded-r-xl">
-        <ul className="flex flex-col gap-2">
+    <div className="fixed inset-0 flex items-center w-full h-full pointer-events-none">
+      <div className="px-2 py-4 bg-popover rounded-r-xl">
+        <ul className="flex flex-col gap-2 pointer-events-auto">
           {FLOATING_NAV_BUTTON.map((item: FloatingNavButtonInterface) => {
             return <FloatingNavButton {...item} />;
           })}
