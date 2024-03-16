bca66087bfe0223dd1f92d6eba1a7f1167ec124a
javiergenepaul <javiergenepaul@gmail.com>
2024-03-16 18:05:25 +0800

    add image in about me section and download resume function

--- src/screens/about/sections/intro-section.tsx
+++ src/screens/about/sections/intro-section.tsx
@@ -1,3 +1,42 @@
+import { AboutMe, ResumeDark } from "@/assets";
+import { Button } from "@/components";
+import { SubTitleAnimation } from "@/screens";
+import { DownloadIcon } from "@radix-ui/react-icons";
+
 export const IntroSection = () => {
-  return <div className="h-screen py-10 bg-blue-950">intro section</div>;
+  const DownloadResumeHandler = () => {
+    console.log("download resume here");
+    window.open(ResumeDark, "_blank");
+  };
+
+  return (
+    <div className="h-screen flex">
+      <div className="basis-1/2 flex items-end pl-24">
+        <img
+          width={"400px"}
+          height={"400px"}
+          src={AboutMe}
+          alt="Gene Paul Mar Javier"
+        />
+      </div>
+      <div className="basis-1/2 flex">
+        <div className="flex flex-col justify-center">
+          <h3 className="font-medium text-2xl">Hello I'm</h3>
+          <h1 className="text-primary text-5xl font-bold">
+            Gene Paul Mar Javier
+          </h1>
+          <p>
+            <SubTitleAnimation />
+          </p>
+          <Button
+            onClick={DownloadResumeHandler}
+            className="space-x-2 w-fit px-8 mt-4"
+          >
+            <span>Download My Resume</span>
+            <DownloadIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
