681c170e55a4aa6eae1002abe242b0fd4cdfd611
javiergenepaul <javiergenepaul@gmail.com>
2024-03-17 12:02:55 +0800

    add border in floating navbar for visibility in light mode

--- src/components/floating-navbar/floating-navbar.tsx
+++ src/components/floating-navbar/floating-navbar.tsx
@@ -113,7 +113,7 @@ export const FloatingNavbar = () => {
       )}
     >
       <div className="fixed inset-0 z-[999999999] flex items-center w-full h-full pointer-events-none">
-        <nav className="px-2 py-4 bg-popover rounded-r-xl">
+        <nav className="px-2 py-4 border bg-popover rounded-r-xl">
           <ul className="flex flex-col gap-2 pointer-events-auto">
             {FLOATING_NAV_BUTTON.map((item: FloatingNavButtonInterface) => {
               return <FloatingNavButton {...item} />;
