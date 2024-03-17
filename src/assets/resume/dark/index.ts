c1aef8fc3698f265fe307da244398e5ad275f25d
javiergenepaul <javiergenepaul@gmail.com>
2024-03-17 12:17:57 +0800

    add dark resume for all color palette registered

---
+++ src/assets/resume/dark/index.ts
@@ -0,0 +1,17 @@
+import ResumeDarkAzureBliss from "./gene-paul-mar-javier-resume-dark-azure-bliss.pdf";
+import ResumeDarkEmeraldElegance from "./gene-paul-mar-javier-resume-dark-emerald-elegance.pdf";
+import ResumeDarkGoldenHaze from "./gene-paul-mar-javier-resume-dark-golden-haze.pdf";
+import ResumeDarkPurpleMajesty from "./gene-paul-mar-javier-resume-dark-purple-majesty.pdf";
+import ResumeDarkScarletSerenade from "./gene-paul-mar-javier-resume-dark-scarlet-serenade.pdf";
+import ResumeDarkSilverSerenity from "./gene-paul-mar-javier-resume-dark-silver-serenity.pdf";
+import ResumeDarkSunsetEmber from "./gene-paul-mar-javier-resume-dark-sunset-ember.pdf";
+
+export {
+  ResumeDarkAzureBliss,
+  ResumeDarkEmeraldElegance,
+  ResumeDarkGoldenHaze,
+  ResumeDarkPurpleMajesty,
+  ResumeDarkScarletSerenade,
+  ResumeDarkSilverSerenity,
+  ResumeDarkSunsetEmber,
+};
