489ef272db6f992551bd0b3d23b4329dfe55e63b
javiergenepaul <javiergenepaul@gmail.com>
2024-03-17 11:39:52 +0800

    using appropriate html tags

--- src/screens/about/sections/experience-section.tsx
+++ src/screens/about/sections/experience-section.tsx
@@ -1,3 +1,5 @@
 export const ExperienceSection = () => {
-  return <div className="h-screen py-10 bg-red-950">ExperienceSection</div>;
+  return (
+    <section className="h-screen py-10 bg-red-950">ExperienceSection</section>
+  );
 };
--- src/screens/about/sections/index.ts
+++ src/screens/about/sections/index.ts
@@ -1,3 +1,4 @@
 export * from "./certificate-section";
 export * from "./intro-section";
 export * from "./experience-section";
+export * from "./language-section";
