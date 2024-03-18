45867dfb29073afde63e6eee582716926bd77cad
javiergenepaul <javiergenepaul@gmail.com>
2024-03-18 12:48:00 +0800

    add manual scroll restoration in about page

--- src/screens/about/about.tsx
+++ src/screens/about/about.tsx
@@ -5,8 +5,14 @@ import {
   IntroSection,
   LanguageSection,
 } from "./sections";
+import { useEffect } from "react";
 
 export const About = () => {
+  
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
+
   return (
     <FadeAnimation>
       <div className="h-full pb-16 space-y-6">
