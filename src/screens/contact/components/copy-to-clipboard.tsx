d7283d605e059d31dd1b3d40babd09ee51df25ce
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-05-16 10:56:36 +0800

    add copy to clipboard feature

---
+++ src/screens/contact/components/copy-to-clipboard.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components";
+import { Clipboard } from "lucide-react";
+import { useState } from "react";
+
+interface CopyToClipBoardInterface {
+  icon: React.ReactNode;
+  content: string;
+  clipboardContent: string;
+}
+
+export const CopyToClipBoard = ({
+  icon,
+  content,
+  clipboardContent,
+}: CopyToClipBoardInterface) => {
+  const [isTooltipOpen, setIsTooltipOpen] = useState<boolean>(false);
+  const [tooltipContent, setTooltipContent] =
+    useState<string>("Copy to ClipBoard");
+
+  return (
+    <div className="flex items-center space-x-2">
+      {icon}
+      <div className="w-fit relative text-nowrap rounded-md border border-input overflow-hidden bg-background pr-16 px-3 py-2 text-sm select-none">
+        {content}
+        <TooltipProvider>
+          <Tooltip open={isTooltipOpen}>
+            <TooltipTrigger asChild>
+              <Button
+                onMouseEnter={() => setIsTooltipOpen(true)}
+                onMouseLeave={() => {
+                  setIsTooltipOpen(false);
+                  setTimeout(() => {
+                    setTooltipContent("Copy to Clipboard");
+                  }, 300); // Delay of 300ms
+                }}
+                variant="secondary"
+                className="absolute rounded-l-8 h-full right-0 top-0 shrink-0 hover:cursor-copy"
+                size={"icon"}
+                onClick={() => {
+                  setIsTooltipOpen(true);
+                  setTooltipContent(clipboardContent);
+                  navigator.clipboard.writeText(content);
+                }}
+              >
+                <Clipboard />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <span>{tooltipContent}</span>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
