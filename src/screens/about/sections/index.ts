eedc5d75a180a4b60528fd31b6233ec180159671
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-05-16 22:21:05 +0800

    feat:add experience section content

--- src/screens/about/sections/index.ts
+++ src/screens/about/sections/index.ts
@@ -2,3 +2,5 @@ export * from "./certificate-section";
 export * from "./intro-section";
 export * from "./experience-section";
 export * from "./language-section";
+export * from "./testimonial-section";
+export * from "./footer-section";
