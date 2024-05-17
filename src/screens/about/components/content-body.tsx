f5e666cd7503b906093f7c3f819eb3d01d483f32
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-05-17 10:32:27 +0800

    feat: add content for education background

--- src/screens/about/components/content-body.tsx
+++ src/screens/about/components/content-body.tsx
@@ -2,21 +2,40 @@ import { Moment } from "moment";
 
 interface ContentBodyProps {
   title: string;
-  subtitle: string;
+  subtitle?: string;
   startYear: Moment;
   endYear: Moment | "present";
+  level: "tertiary" | "secondary" | "primary" | "vocational";
+  abbreviation: string;
 }
 
 export const ContentBody = (props: ContentBodyProps) => {
-  const { title, subtitle, startYear, endYear } = props;
+  const { title, subtitle, startYear, endYear, abbreviation, level } = props;
   return (
     <article className="flex flex-col gap-2 border-b-gray-500">
-      {title}
-      <div className="flex justify-between">
-        {/* subtitle */}
-        <div className="">{subtitle}</div>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          {/* title */}
+          <div className="flex gap-2">
+            <span className="font-bold">{title}</span>
+            <div className="font-light">{`(${abbreviation})`}</div>
+          </div>
+
+          {/* subtitle */}
+          {subtitle && (
+            <div className="font-medium text-foreground">{subtitle}</div>
+          )}
+
+          {/* level */}
+          <div className="font-extralight font-xs capitalize">{level}</div>
+        </div>
         {/* year */}
-        {/* <div className="">{startYear} </div> */}
+        <div className="flex gap-1">
+          <span>{startYear.format("YYYY")}</span>~
+          <span>
+            {endYear === "present" ? "present" : endYear.format("YYYY")}
+          </span>
+        </div>
       </div>
     </article>
   );
