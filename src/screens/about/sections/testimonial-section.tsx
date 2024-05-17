d4b402678b79d2443233183a5a170d8a74fd7869
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-05-17 12:28:47 +0800

    add banner to all section in about us

--- src/screens/about/sections/testimonial-section.tsx
+++
@@ -1,3 +0,0 @@
-export const TestimonialSection = () => {
-  return <section className="h-screen py-10 bg-pink-950">Testimonial Section</section>;
-};
---
+++ src/screens/about/sections/testimonial-section/testimonial-section.tsx
@@ -0,0 +1,12 @@
+import { Banner } from "../../components";
+
+export const TestimonialSection = () => {
+  return (
+    <div className="relative pb-16">
+      <Banner />
+      <section className="h-screen py-10 bg-pink-950">
+        Testimonial Section
+      </section>
+    </div>
+  );
+};
