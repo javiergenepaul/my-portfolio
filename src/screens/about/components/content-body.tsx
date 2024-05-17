50de943982f4270f0da196221fbc86dff50f8fb2
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-05-17 11:41:38 +0800

    adding design layout for experience section in about page

--- src/screens/about/components/content-body.tsx
+++ src/screens/about/components/content-body.tsx
@@ -1,42 +1,91 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components";
+import { Dot } from "lucide-react";
+import moment from "moment";
 import { Moment } from "moment";
+import "moment-duration-format";
 
 interface ContentBodyProps {
   title: string;
   subtitle?: string;
   startYear: Moment;
   endYear: Moment | "present";
-  level: "tertiary" | "secondary" | "primary" | "vocational";
-  abbreviation: string;
+  level?: "tertiary" | "secondary" | "primary" | "vocational";
+  abbreviation?: string;
+  isWork?: boolean;
 }
 
 export const ContentBody = (props: ContentBodyProps) => {
-  const { title, subtitle, startYear, endYear, abbreviation, level } = props;
-  return (
-    <article className="flex flex-col gap-2 border-b-gray-500">
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col">
-          {/* title */}
-          <div className="flex gap-2">
-            <span className="font-bold">{title}</span>
-            <div className="font-light">{`(${abbreviation})`}</div>
-          </div>
+  const { title, subtitle, startYear, endYear, abbreviation, level, isWork } =
+    props;
 
-          {/* subtitle */}
+  const getYearSpent = (
+    startDate: Moment,
+    endDate: Moment | "present"
+  ): string => {
+    let years;
+    let months;
+
+    if (endDate === "present") {
+      years = moment().diff(startDate, "years");
+      months = moment().diff(startDate, "months");
+    } else {
+      years = endDate.diff(startDate, "years");
+      months = endDate.diff(startDate, "months");
+    }
+
+    months = months % 12;
+
+    let yearSpentText: string = "";
+
+    if (years !== 0) {
+      yearSpentText = yearSpentText + `${years} ${years > 1 ? "yrs" : "yr"}`;
+    }
+
+    if (months !== 0) {
+      if (years !== 0) {
+        yearSpentText = yearSpentText + " & ";
+      }
+      yearSpentText = yearSpentText + `${months} ${months > 1 ? "mos" : "mo"}`;
+    }
+
+    return yearSpentText;
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <span className="font-bold">{title}</span>
+        </CardTitle>
+        <CardDescription>
           {subtitle && (
             <div className="font-medium text-foreground">{subtitle}</div>
           )}
-
-          {/* level */}
-          <div className="font-extralight font-xs capitalize">{level}</div>
-        </div>
-        {/* year */}
-        <div className="flex gap-1">
-          <span>{startYear.format("YYYY")}</span>~
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex gap-1 text-nowrap text-xs items-center">
+          <span>{startYear.format(isWork ? "MMM YYYY" : "YYYY")}</span>~
           <span>
-            {endYear === "present" ? "present" : endYear.format("YYYY")}
+            {endYear === "present"
+              ? "present"
+              : endYear.format(isWork ? "MMM YYYY" : "YYYY")}
           </span>
+          {/* years spent */}
+          {isWork && (
+            <>
+              <Dot></Dot>
+              <div className="">{getYearSpent(startYear, endYear)}</div>
+            </>
+          )}
         </div>
-      </div>
-    </article>
+      </CardContent>
+    </Card>
   );
 };
--- src/screens/about/sections/experience-section/components/experience-content.tsx
+++ src/screens/about/sections/experience-section/components/experience-content.tsx
@@ -1,10 +1,45 @@
 import { translate } from "@/i18n";
-import { ContentTitle } from "@/screens";
+import { ContentBody, ContentTitle } from "@/screens";
+import moment from "moment";
 
 export const ExperienceContent = () => {
   return (
     <section className="basis-1/2">
       <ContentTitle title={translate("about.experience.title")} />
+
+      <div className="flex flex-col gap-4">
+        <ContentBody
+          title={"Software Engineer - Associate Technical Specialist I"}
+          subtitle={"Alliance Software Inc."}
+          startYear={moment("2024-01-01")}
+          abbreviation="ATS I"
+          endYear="present"
+          isWork
+        />
+        <ContentBody
+          title={"Software Engineer - Technical Specialist I"}
+          subtitle={"Alliance Software Inc."}
+          startYear={moment("2022-12-01")}
+          abbreviation="TS I"
+          endYear={moment("2024-01-01")}
+          isWork
+        />
+        <ContentBody
+          title={"Software Develop"}
+          subtitle={"Mach95 Software Development Corporation"}
+          startYear={moment("2021-07-01")}
+          endYear={moment("2022-12-01")}
+          isWork
+        />
+        <ContentBody
+          title={"VR/AR Developer & 3D Modeler"}
+          subtitle={"Exodia Game Development"}
+          abbreviation="Part Time"
+          startYear={moment("2019-07-01")}
+          endYear={moment("2021-07-01")}
+          isWork
+        />
+      </div>
     </section>
   );
 };
