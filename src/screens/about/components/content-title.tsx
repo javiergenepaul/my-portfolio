96281a1dec6a5a288d95b458dbcf9b0420231aaf
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-06-02 17:59:55 +0800

    add certificate section & simplify background section

--- src/screens/about/components/content-title.tsx
+++ src/screens/about/components/content-title.tsx
@@ -4,5 +4,5 @@ interface ContentTitleInterface {
 
 export const ContentTitle = (props: ContentTitleInterface) => {
   const { title } = props;
-  return <div className="text-2xl font-bold">{title}</div>;
+  return <div className="text-2xl font-bold pb-4">{title}</div>;
 };
