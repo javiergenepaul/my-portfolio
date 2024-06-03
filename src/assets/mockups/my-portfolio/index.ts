19b579cccc9920f22c7580d54c1bd13fa6e3a757
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-06-03 10:09:29 +0800

    add mockups for my-portfolio

---
+++ src/assets/mockups/my-portfolio/index.ts
@@ -0,0 +1,7 @@
+import MyPortfolio1 from "./my-portfolio-mockup-1.png";
+import MyPortfolio2 from "./my-portfolio-mockup-2.png";
+import MyPortfolio3 from "./my-portfolio-mockup-3.png";
+import MyPortfolio4 from "./my-portfolio-mockup-4.png";
+import MyPortfolio5 from "./my-portfolio-mockup-5.png";
+
+export { MyPortfolio1, MyPortfolio2, MyPortfolio3, MyPortfolio4, MyPortfolio5 };
