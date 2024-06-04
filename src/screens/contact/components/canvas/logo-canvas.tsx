0dafa3f3be57651c13794b8a3e41235341934d2c
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-06-04 11:24:41 +0800

    change ico

--- src/screens/contact/components/canvas/logo-canvas.tsx
+++ src/screens/contact/components/canvas/logo-canvas.tsx
@@ -109,6 +109,7 @@ export const LogoCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          enableRotate={true}
         />
       </Suspense>
       <FollowCameraLight />
