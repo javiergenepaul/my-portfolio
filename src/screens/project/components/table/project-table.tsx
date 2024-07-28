a8e221dd4bb03728fdf8da920598421b5f73ecd8
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-28 10:39:37 +0800

    add view mode

--- src/screens/project/components/table/project-table.tsx
+++ src/screens/project/components/table/project-table.tsx
@@ -33,7 +33,7 @@ export const ProjectTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {PROJECTS.filter((project) => !project.hidden).map((project) => (
+        {PROJECTS.filter((project) => !project.hidden).map((project, key) => (
           <TableRow
             className={
               project.type !== "confidential" && project.previewUrl
