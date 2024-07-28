fc966109977bc42d727f6a8aa5699d7078a64efc
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-28 10:16:32 +0800

    add pawfectly yours project

--- src/screens/project/components/table/project-table.tsx
+++ src/screens/project/components/table/project-table.tsx
@@ -33,7 +33,7 @@ export const ProjectTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {PROJECTS.map((project) => (
+        {PROJECTS.filter((project) => !project.hidden).map((project) => (
           <TableRow
             className={
               project.type !== "confidential" && project.previewUrl
