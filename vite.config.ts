9b83914fe87eaf3fa7dfdbc1e11f023e6226bff5
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-05 01:00:12 +0800

    feat: add obfuscate in the tailwind css class to encrypt tailwind class

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import utwm from "unplugin-tailwindcss-mangle/vite"; // Add this import
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), utwm()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
