feb7f310059e7d2f895a5aac23e8938019dcb1fe
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-05 01:05:52 +0800

    update condition to check if dev mode or prod mode to enable and disable obfuscate

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import react from "@vitejs/plugin-react";
 import utwm from "unplugin-tailwindcss-mangle/vite"; // Add this import
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), utwm()],
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    ...(mode === "production" ? [utwm()] : []), // Only enable in production
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
