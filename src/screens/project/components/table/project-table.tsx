c58b15efe57c569a5f2c5b84f2ac6998041a3f40
GPM <javiergenepaul@gmail.com>
2026-04-13 00:32:08 +0800

    feat: update the years experience in year 2026

--- src/screens/project/components/table/project-table.tsx
+++ src/screens/project/components/table/project-table.tsx
@@ -33,96 +33,98 @@ export const ProjectTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {getProjects().filter((project) => !project.hidden).map((project) => (
-          <TableRow
-            className={
-              project.type !== "confidential" && project.previewUrl
-                ? "hover:cursor-pointer"
-                : "hover:cursor-not-allowed"
-            }
-            onDoubleClick={() => {
-              window.open(project.previewUrl, "_blank");
-            }}
-            key={project.projectId}
-          >
-            {/* TITLE */}
-            <TableCell className="font-medium">{project.title}</TableCell>
-            {/* COMPANY */}
-            <TableCell>
-              {project.company ? project.company : "Personal"}
-            </TableCell>
-            {/* CATEGORY */}
-            <TableCell className="flex gap-2 flex-wrap">
-              {project.category.map((category) => {
-                return <Badge key={category}>{category}</Badge>;
-              })}
-            </TableCell>
-            {/* TYPE */}
-            <TableCell className="capitalize">{project.type}</TableCell>
-            {/* STATUS */}
-            <TableCell className="capitalize">{project.status}</TableCell>
-            {/* ACTION */}
-            <TableCell className="flex gap-2 w-fit">
-              {project.type !== "confidential" ? (
-                <>
-                  {project.previewUrl && (
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <IndicatorContainer>
-                            <Button
-                              size={"icon"}
-                              onClick={() => {
-                                window.open(project.previewUrl, "_blank");
-                              }}
-                            >
-                              <Eye width="1.2rem" height="1.2rem" />
-                            </Button>
-                          </IndicatorContainer>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <span>
-                            {translate("projects.indicator.demoAvailable", {
-                              title: project.title,
-                            })}
-                          </span>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  )}
+        {getProjects()
+          .filter((project) => !project.hidden)
+          .map((project) => (
+            <TableRow
+              className={
+                project.type !== "confidential" && project.previewUrl
+                  ? "hover:cursor-pointer"
+                  : "hover:cursor-not-allowed"
+              }
+              onDoubleClick={() => {
+                window.open(project.previewUrl, "_blank");
+              }}
+              key={project.projectId}
+            >
+              {/* TITLE */}
+              <TableCell className="font-medium">{project.title}</TableCell>
+              {/* COMPANY */}
+              <TableCell>
+                {project.company ? project.company : "Personal"}
+              </TableCell>
+              {/* CATEGORY */}
+              <TableCell className="flex gap-2 flex-wrap">
+                {project.category.map((category) => {
+                  return <Badge key={category}>{category}</Badge>;
+                })}
+              </TableCell>
+              {/* TYPE */}
+              <TableCell className="capitalize">{project.type}</TableCell>
+              {/* STATUS */}
+              <TableCell className="capitalize">{project.status}</TableCell>
+              {/* ACTION */}
+              <TableCell className="flex gap-2 w-fit">
+                {project.type !== "confidential" ? (
+                  <>
+                    {project.previewUrl && (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <IndicatorContainer>
+                              <Button
+                                size={"icon"}
+                                onClick={() => {
+                                  window.open(project.previewUrl, "_blank");
+                                }}
+                              >
+                                <Eye width="1.2rem" height="1.2rem" />
+                              </Button>
+                            </IndicatorContainer>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <span>
+                              {translate("projects.indicator.demoAvailable", {
+                                title: project.title,
+                              })}
+                            </span>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    )}
 
-                  {project.codeUrl && (
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <IndicatorContainer>
-                            <Button
-                              size={"icon"}
-                              onClick={() => {
-                                window.open(project.codeUrl, "_blank");
-                              }}
-                            >
-                              <Code width="1.2rem" height="1.2rem" />
-                            </Button>
-                          </IndicatorContainer>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <span>
-                            {translate("projects.indicator.codeAvaialble", {
-                              title: project.title,
-                            })}
-                          </span>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  )}
-                </>
-              ) : (
-                "-"
-              )}
-            </TableCell>
-          </TableRow>
-        ))}
+                    {project.codeUrl && (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <IndicatorContainer>
+                              <Button
+                                size={"icon"}
+                                onClick={() => {
+                                  window.open(project.codeUrl, "_blank");
+                                }}
+                              >
+                                <Code width="1.2rem" height="1.2rem" />
+                              </Button>
+                            </IndicatorContainer>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <span>
+                              {translate("projects.indicator.codeAvaialble", {
+                                title: project.title,
+                              })}
+                            </span>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    )}
+                  </>
+                ) : (
+                  "-"
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
       </TableBody>
     </Table>
   );
